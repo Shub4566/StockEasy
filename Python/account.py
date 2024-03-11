89d6b000d7bcddcc0fac0ccaf330a27f7b939af5
Mohit Kumar <85069851+iMohitKumarSingh@users.noreply.github.com>
2024-03-11 13:19:23 +0530

    Update account.py

--- Python/account.py
+++ Python/account.py
@@ -1,11 +1,7 @@
 import streamlit as st
 import firebase_admin
 
-from firebase_admin import credentials
-from firebase_admin import auth
-cred = credentials.Certificate('stockeasy-f3ac0-140c00ab9b02.json')
-firebase_admin.initialize_app(cred)
-    
+
 
 
 
