dcd07fbd96c7769cae8371f91b4cb253572b8330
Mohit Kumar <85069851+iMohitKumarSingh@users.noreply.github.com>
2024-03-12 21:52:01 +0530

    Update account.py

--- Python/account.py
+++ Python/account.py
@@ -6,7 +6,7 @@ from firebase_admin import auth
 
 
 cred = credentials.Certificate('stockeasy-f3ac0-140c00ab9b02.json')
-# firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred)
 
 
 
